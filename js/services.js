cf6f7a4f67cc191f5692cab8759be39ea9b62c33
saleem_cmp <saleem.u@compassitesinc.com>
2016-02-20 17:17:55 +0530

    addded new push notif

--- js/services.js
+++ js/services.js
@@ -4,6 +4,10 @@ angular.module('app.services', [])
 
 }])
 
+.factory('userInfo', ['$resource',function($resource){
+	return $resource('http://jsonplaceholder.typicode.com/users');
+}])
+
 .service('BlankService', [function(){
 
 }]);
