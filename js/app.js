289f5e1c101ae236db9b7a434105b18e00fb5050
sumanth <sumanth.ece52@gmail.com>
2016-02-22 20:14:50 +0530

    Partial form Validations

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic','ionic.service.core','ionic.service.analytics', 'app.controllers', 'app.routes', 'app.services', 'app.directives','ngResource'])
+angular.module('app', ['ionic','ionic.service.core','ionic.service.analytics', 'app.controllers', 'app.routes', 'app.services', 'app.directives','ngResource', 'ngMessages'])
 
 .run(function($ionicPlatform,$ionicAnalytics) {
   $ionicPlatform.ready(function() {
