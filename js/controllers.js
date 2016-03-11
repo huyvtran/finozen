658914dc00c3d4b2a5fa07837ce93d93b4a34333
saleem_cmp <saleem.u@compassitesinc.com>
2016-03-11 18:54:25 +0530

    edited new controller files

--- js/controllers.js
+++ js/controllers.js
@@ -634,3 +634,13 @@ $sessionStorage.xirr=data.jsonStr.xirr;
     })
 
 
+  .controller('TestCtrl', function($scope, $ionicLoading) {
+
+   $scope.showLoading = function() {
+      $ionicLoading.show();
+   };
+
+   $scope.hideLoading = function(){
+      $ionicLoading.hide();
+   };
+});
--- js/subcontrollerTwo.js
+++ js/subcontrollerTwo.js
@@ -1,12 +1,2 @@
 angular.module('app.subcontrollerTwo', [])
 
-  .controller('TestCtrl', function($scope, $ionicLoading) {
-
-   $scope.showLoading = function() {
-      $ionicLoading.show();
-   };
-
-   $scope.hideLoading = function(){
-      $ionicLoading.hide();
-   };
-});
