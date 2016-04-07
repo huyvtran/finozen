8b540003ca564a4bfa6d97bf9493c97b0d617fdc
piyushdutta <piyush1234dutta@gmail.com>
2016-04-07 15:58:31 +0530

    added image selection page

--- js/controllers.js
+++ js/controllers.js
@@ -88,7 +88,6 @@ $sessionStorage.SessionMobNo=signupForm.mobileNumber;
        $scope.sendSignIn();
         }
       }
-
     $scope.forgotPin=function(signinformData){
   if(signinformData.$valid){
     $sessionStorage.forgotPinPhone = $scope.authorization.login;
